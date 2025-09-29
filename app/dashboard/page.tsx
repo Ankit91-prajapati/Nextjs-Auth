4a07b8ebc9c6a2846da3c07c97e9c543b47a47cb
Ankit91-prajapati <ankitprajapati6458@gmail.com>
2025-09-29 15:00:30 +0530

    Auth

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+import { auth } from "@/lib/auth";
+import DashboardClientPage from "@/components/DasboardClient";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/auth");
+  }
+
+  return <DashboardClientPage session={session} />;
+}
