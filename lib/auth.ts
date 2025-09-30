d234893ecdd9decdc20364f7a89d69f89ce80c78
Ankit91-prajapati <ankitprajapati6458@gmail.com>
2025-09-30 19:39:34 +0530

    Update

--- lib/auth.ts
+++ lib/auth.ts
@@ -17,10 +17,7 @@ export const auth = betterAuth({
     sendOnSignIn: true,
   },
 
-  session: {
-    expiresIn: 60 * 60 * 24 * 7, // 7 days
-    updateAge: 60 * 60 * 24, // 1 day (every 1 day the session expiration is updated)
-  },
+  
 
   socialProviders: {
     github: {
