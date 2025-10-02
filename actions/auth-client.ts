634c04d4f3d41bc2dcdb87061c6844a198f095cd
Ankit91-prajapati <ankitprajapati6458@gmail.com>
2025-10-02 14:01:16 +0530

    Url

--- actions/auth-client.ts
+++ actions/auth-client.ts
@@ -1,5 +1,5 @@
 import { auth } from './../lib/auth';
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000/api/auth" // The base URL of your auth server
+    baseURL: process.env.BETTER_AUTH_URL as string// The base URL of your auth server
 })
