f651b7cfc24299143ef48828dc06e768c42be9e3
Ankit91-prajapati <ankitprajapati6458@gmail.com>
2025-10-02 13:53:26 +0530

    New

--- actions/auth-client.ts
+++ actions/auth-client.ts
@@ -1,4 +1,5 @@
+import { auth } from './../lib/auth';
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: "http://localhost:3000" // The base URL of your auth server
+    baseURL: "http://localhost:3000/api/auth" // The base URL of your auth server
 })
